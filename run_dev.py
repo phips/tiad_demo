552c88f4fc8d25ef70579728bba0bbac90e15a54
Mark Phillips <mark@probably.co.uk>
2015-03-18 19:26:05 +0000

    Updated listen to be on all interfaces

--- run_dev.py
+++ run_dev.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from app import app
-app.run(debug = True)
+app.run(debug = True, host='0.0.0.0')
--- run_prod.py
+++ run_prod.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from app import app
-app.run(debug = False)
+app.run(debug = False, host='0.0.0.0')
